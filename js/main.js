f75df191fa9e6057009527068b673fc8c9c64e68
anthonylxd <lxdblues@gmail.com>
2017-07-10 17:10:18 +0800

    解决https页面请求http
    iframe

--- js/main.js
+++ js/main.js
@@ -63,18 +63,21 @@ $(function()
 })
 
 
-	//创建XMLHttpRequest
-	var request;
-	if(window.XMLHttpRequest){
-		request = new XMLHttpRequest();
-	}else{
-		request = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	request.open("POST","http://123.206.42.190/research/GetRequest",true);
-	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-	request.send();
-	request.onreadystatechange=function() {
-  		if (request.readyState==4 && request.status==200){
-		var result = request.responseText;
-	}
-}
+// 	//创建XMLHttpRequest
+// 	var request;
+// 	if(window.XMLHttpRequest){
+// 		request = new XMLHttpRequest();
+// 	}else{
+// 		request = new ActiveXObject("Microsoft.XMLHTTP");
+// 	}
+// 	request.open("POST","http://123.206.42.190/research/GetRequest",true);
+// 	request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+// 	request.send();
+// 	request.onreadystatechange=function() {
+//   		if (request.readyState==4 && request.status==200){
+// 		var result = request.responseText;
+// 	}
+// }
+
+// window.open ('http://123.206.42.190/research/GetRequest','newwindow','height=100,width=400,top=0,left=0,toolbar=no,menubar=no,scrollbars=no, resizable=no,location=no, status=no') 
+// 
