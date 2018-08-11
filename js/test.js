3e68cd842e81352d95539e513c2bddd1a0bccad5
anthonylxd <lxdblues@gmail.com>
2018-08-11 09:59:36 +0800

    201808111

--- js/test.js
+++ js/test.js
@@ -13,5 +13,41 @@ $(".leftcontent li").click(function(){
 });
 
 
+	var sidebar = $('#sidebar'),//Ñ¡Ôñ²àÀ¸
+		mask= $('#mask'),
+		sidebar_trigger = $('.sidebar_trigger'),
+		backButton = $('.back-to-top'),
+		itembtn = $('#itembtn'),
+		workbtn = $('#workbtn'),
+		skillbtn = $('#skillbtn');
+		
+		function showSidebar(){
+			mask.fadeIn();
+			//sidebar.animate({'right':0});
+			sidebar.css('right',0);			
+		}
+		function hideSidebar(){
+			mask.fadeOut();
+			sidebar.css('right',-sidebar.width());
+		}
+		sidebar_trigger.on('click',showSidebar)
+		mask.on('click',hideSidebar)
+		
+
+		function backtop(){
+			$('html, body').animate({
+				scrollTop:0
+			},800)
+		}
+		backButton.on('click',backtop);
+		
+		$(window).on('scroll',function(){
+			if($(window).scrollTop()>$(window).height())
+					backButton.fadeIn();
+				else
+					backButton.fadeOut();
+		})
+		
+		$(window).trigger('scroll');
 
 });
